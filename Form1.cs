0f86133fc0b6f6f5229dd4aa755a1e09090d8873
KED JINXS <ericjinxs@gmail.com>
2024-10-03 18:00:58 -0600

    Created buttons all named under the function UsersSelect

--- Form1.cs
+++ Form1.cs
@@ -12,14 +12,40 @@ namespace Tic_Tac_Toe_New_Rapid_Class_SODV2202___Group6
 {
     public partial class Form1 : Form
     {
+        private bool TrackPlayersTurn = true; // Track whose turn it is
+        private int movesCount = 0; // Count moves to detect draw
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void UsersSelect(object sender, EventArgs e)
+        {
+            Button tempButton = (Button)sender;
+
+            if (tempButton.Text == "")
+            {
+                //tempButton.Text = isPlayrsTurn
+                if (TrackPlayersTurn) // first player will always be x (true)
+                {
+                    tempButton.Text = "X";
+                    TrackPlayersTurn = false; // changes to false so that next button is o
+                }
+                else
+                {
+                    tempButton.Text = "O";
+                    TrackPlayersTurn = true;
+                }
+                movesCount++;
+                CheckWinnerBox();
+            }
+        }
+
+        private void CheckWinnerBox()
         {
 
         }
+
+
     }
 }
